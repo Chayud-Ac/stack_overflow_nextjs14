c35b5fd1719c208dcf40ceef7552d47bb023dc42
Chayud-Ac <acieacie@hotmail.com>
2024-07-11 20:00:48 +0700

    edited webhook route in middleware

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,7 +7,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
   // TODO: add WEBHOOK_SECRET in local env
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
@@ -54,7 +54,9 @@ export async function POST(req: Request) {
   // Do something with the payload
   // For this guide, you simply log the payload to the console
   const eventType = evt.type;
+
   console.log({ eventType });
+
   //   console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
   //   console.log('Webhook body:', body)
 
